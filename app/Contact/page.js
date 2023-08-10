39a81ea43ee45331db8c989c636b405be17dd652
Kameron  Farshchian <kfarshchian@gmail.com>
2023-08-10 16:31:16 -0600

    fix navbar

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -1,20 +1,16 @@
-
 import React from "react";
 import "./Contact.scss";
-import {
-  Container
-} from "@mui/material";
+import { Container } from "@mui/material";
 
 const Contact = () => {
   return (
     <div id="Contact">
-     <Container id="serviceHeader">
-          <h1 id="web-development-SEO-management-contact">
-            Contact Us
-          </h1>
-      </Container>
+      <div id="ContactImage">
+        <h1 id="web-development-SEO-management-contact">Contact Us</h1>
+      </div>
+      <Container id="serviceHeader"></Container>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- components/MobileNav.js
+++ components/MobileNav.js
@@ -47,19 +47,19 @@ const Navbar = () => {
               }`}
             >
               <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-              <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900   md:hover:bg-transparent">
+              <li className="pb-6 text-l text-white py-2 md:px-0 text-center border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900   md:hover:bg-transparent">
                     <Link id="pageLink" href="/About" onClick={() => setNavbar(!navbar)}> About Us</Link>
                   </li>
-                  <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900  md:hover:bg-transparent">
+                  <li className="pb-6 text-l text-white py-2 px-0 text-center  border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900  md:hover:bg-transparent">
                     <Link id="pageLink" href="/Our-Work" onClick={() => setNavbar(!navbar)}>Our Work</Link>
                   </li>
-                  <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900 md:hover:bg-transparent">
+                  <li className="pb-6 text-l text-white py-2 px-0 text-center  border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900 md:hover:bg-transparent">
                     <Link id="pageLink" href="/Services" onClick={() => setNavbar(!navbar)}>Services</Link>
                   </li>
-                  <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900 md:hover:bg-transparent">
+                  <li className="pb-6 text-l text-white py-2 px-0 text-center  border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900 md:hover:bg-transparent">
                     <Link id="pageLink" href="tel:801-574-1949">801-574-1949</Link>
                   </li>
-                  <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900 md:hover:bg-transparent">
+                  <li className="pb-6 text-l text-white py-2 px-0 text-center  border-b-2 md:border-b-0  hover:bg-slate-600  border-white-900 md:hover:bg-transparent">
                     <Link id="pageLink" href="/Contact" onClick={() => setNavbar(!navbar)}>Contact</Link>
                   </li>
               </ul>
