370abda87e484cb139bacfab662c646b4d4d5131
Kameron  Farshchian <kfarshchian@gmail.com>
2023-08-09 18:59:08 -0600

    some of the service page

--- app/Services/page.js
+++ app/Services/page.js
@@ -1,21 +1,65 @@
-
 import React from "react";
 import "./Services.scss";
-import {
-  Container
-} from "@mui/material";
+import { Container } from "@mui/material";
+import SlideIn from "@/components/SlideIn/SlideIn";
 
 const Services = () => {
   return (
-    <div id="Pricing">
-      <h1 id="web-development-SEO-management-services">
-            Our Services
-          </h1>
-     <Container id="section1">
-          
+    <>
+      <div id="Pricing">
+        <h1 id="web-development-SEO-management-services">Our Services</h1>
+      </div>
+      <Container>
+        <p id="web-development-SEO-management-services-P">
+          Our comprehensive suite of services encompasses a diverse range of
+          essential elements, designed to elevate your online presence and drive
+          sustainable growth. With a meticulous approach to Web Development, we
+          craft tailored digital experiences that seamlessly merge aesthetic
+          appeal with exceptional functionality. Our team's proficiency in
+          leveraging cutting-edge technologies ensures that your website not
+          only captivates visitors but also provides an intuitive user journey,
+          converting leads into loyal customers.
+        </p>
+        <p id="web-development-SEO-management-services-P">
+          In the realm of Search Engine Optimization (SEO), we employ a
+          strategic approach to enhance your website's visibility in search
+          engine results. Our adept SEO specialists meticulously analyze your
+          digital footprint, strategically implement keywords, optimize meta
+          tags, and curate engaging content to catapult your website to the
+          forefront of relevant search queries. Additionally, our SEO Site
+          Audits provide a comprehensive assessment of your website's
+          performance, identifying areas for improvement and tailoring
+          actionable recommendations to enhance its overall SEO efficacy.
+        </p>
+        <p id="web-development-SEO-management-services-P">
+          At the heart of our offerings lies Branding and Design, where we
+          conceptualize, refine, and revitalize brands to communicate their core
+          essence cohesively across various touchpoints. Our creative team
+          meticulously crafts visual identities that resonate with your target
+          audience, establishing a lasting imprint that fosters brand
+          recognition and loyalty.
+        </p>
+        <p id="web-development-SEO-management-services-P">
+          Our adept Social Media content strategy bridges the gap between your
+          brand and its audience, effectively leveraging platforms to
+          disseminate compelling narratives, engage followers, and foster
+          meaningful connections. Simultaneously, our Content Creation services
+          breathe life into your digital presence, crafting captivating
+          narratives, informative articles, and visually striking assets that
+          resonate with your audience and position you as an industry authority.
+        </p>
+        <p id="web-development-SEO-management-services-P">
+          In essence, our comprehensive array of services synergistically
+          collaborates to forge a robust and captivating online identity,
+          enabling you to leave an indelible mark on your industry while
+          fostering meaningful customer engagement and lasting success.
+        </p>
       </Container>
-    </div>
-  )
-}
+      <Container id="section1">
+        <SlideIn />
+      </Container>
+    </>
+  );
+};
 
-export default Services
+export default Services;
---
+++ components/SlideIn/SlideIn.js
@@ -0,0 +1,173 @@
+"use client";
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { Stack } from "@mui/material";
+import "./SlideIn.scss";
+import WebIcon from "@mui/icons-material/Web";
+import StorageIcon from "@mui/icons-material/Storage";
+import PageviewIcon from "@mui/icons-material/Pageview";
+
+const SlideIn = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    observer.observe(divRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <>
+      <Stack
+        direction={{ xs: "column", sm: "column" }}
+        id="web-development-SEO-management-stack-column"
+        sx={{
+          marginTop: "50px",
+          marginBottom: "50px",
+          paddingLeft: "5px",
+          paddingRight: "5px",
+        }}
+        spacing={{ xs: 1, sm: 1, md: 6 }}
+      >
+        <div
+          ref={divRef}
+          className={isVisible ? "slide-in" : ""}
+          id="web-development-SEO-management-stack-column-box"
+        >
+          <WebIcon
+            sx={{
+              fontSize: "200px",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "Black",
+              backgroundColor: "white",
+              backgroundColor: "transparent",
+            }}
+          />
+          <div id="web-development-SEO-management-H2-P">
+            <h2 id="web-development-SEO-management-slideInH2">React Website</h2>
+            <p id="web-development-SEO-management-slideInP">
+              A React website is a web application developed using the React
+              JavaScript library, employing a component-based approach for
+              building reusable UI elements. Its dynamic nature and efficient
+              rendering make it a favored choice for creating interactive and
+              responsive web interfaces.
+            </p>
+
+            <p id="web-development-SEO-management-slideInP">
+              React itself doesn't inherently hinder SEO, but it requires
+              careful consideration and implementation to ensure good SEO
+              performance. Traditional search engine crawlers might struggle
+              with fully indexing single-page applications (SPAs) built with
+              React due to their dynamic nature, which can affect SEO if not
+              addressed properly. It is generally better to go with another
+              library if you need optimum SEO performance.
+            </p>
+            <p id="web-development-SEO-management-slideInP">
+              1375.00 landing page and 5 additional pages.
+            </p>
+            <p id="web-development-SEO-management-slideInP">
+              Domain and hosting fee 29.99/ monthly
+            </p>
+          </div>
+        </div>
+        <div
+          ref={divRef}
+          className={isVisible ? "slide-in2" : ""}
+          id="web-development-SEO-management-stack-column-box"
+        >
+          <div id="web-development-SEO-management-H2-P">
+            <h2 id="web-development-SEO-management-slideInH2">
+              Next.js Website
+            </h2>
+            <p id="web-development-SEO-management-slideInP">
+              Next.js is a popular open-source framework for building React
+              applications with a focus on server-side rendering (SSR) and
+              static site generation (SSG), which improves performance and SEO.
+              It provides a structured approach to creating dynamic and
+              SEO-friendly web applications, integrating features like automatic
+              code splitting, routing, and server-side rendering out of the box.
+              Because of the server running Next.js the monthly hosting fee
+              costs a little extra to host.
+            </p>
+            <p id="web-development-SEO-management-slideInP">
+              With us Next.js offers the advantage of waiving the SEO starting
+              fee, providing an incentive for businesses seeking improved
+              visibility and search engine rankings.
+            </p>
+            <p id="web-development-SEO-management-slideInP">
+              1550.00 landing page and 5 additional pages.
+            </p>
+            <p id="web-development-SEO-management-slideInP">
+              Domain and hosting fee 49.99
+            </p>
+          </div>
+          <StorageIcon
+            sx={{
+              fontSize: "200px",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "Black",
+              backgroundColor: "white",
+              backgroundColor: "transparent",
+            }}
+          />
+        </div>
+        <div
+          ref={divRef}
+          className={isVisible ? "slide-in3" : ""}
+          id="web-development-SEO-management-stack-column-box"
+        >
+          <PageviewIcon
+            sx={{
+              fontSize: "200px",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "Black",
+              backgroundColor: "transparent",
+            }}
+          />
+          <div id="web-development-SEO-management-H2-P">
+            <h2 id="web-development-SEO-management-slideInH2">
+              SEO (Search Engine Optimization)
+            </h2>
+            <p id="web-development-SEO-management-slideInP">
+              SEO, or Search Engine Optimization, is the practice of optimizing
+              a website's content, structure, and technical elements to improve
+              its visibility and ranking in search engine results, ultimately
+              driving organic (non-paid) traffic and enhancing online presence.
+            </p>
+            <p id="web-development-SEO-management-slideInP">
+              SEO efforts require ongoing monthly management to maintain
+              and improve a website's search engine visibility. This involves
+              monitoring keyword rankings, analyzing website traffic and user
+              behavior, adjusting content, building backlinks, and making
+              technical optimizations to adapt to search engine algorithm
+              changes and stay competitive in search results.
+            </p>
+            <p id="web-development-SEO-management-slideInP">
+             SEO audit price $500.00 to $2500.00, contingent upon the scale and complexity of the website in question. 
+            </p>
+            <p id="web-development-SEO-management-slideInP">
+            The monthly management price for SEO services ranges from $500.00 to $1500.00, contingent upon the scale and complexity of the website in question.
+            </p>
+          </div>
+        </div>
+      </Stack>
+    </>
+  );
+};
+
+export default SlideIn;
